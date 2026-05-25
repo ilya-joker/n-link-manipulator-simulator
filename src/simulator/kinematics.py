654d5a13394484a60aec8e04d0cbcc97d619f887
Ilya Koloshin <ilya.koloshin@gmail.com>
2026-05-26 00:34:04 +0400

    add rotation_matrix, tranform_matrix and forward_kinematics

---
+++ src/simulator/kinematics.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def rotation_matrix(angle_deg):
+    Tetta = np.radians(angle_deg)
+    T = np.array([
+        [np.cos(Tetta), -np.sin(Tetta)],
+        [np.sin(Tetta), np.cos(Tetta)]
+    ])
+    return T
+
+def transform_matrix(angle_deg, tx, ty):
+    T = np.eye(3)  # единичная матрица 3×3
+    T[:2, :2] = rotation_matrix(angle_deg)  # вставить матрицу поворота в верхний левый угол
+    T[0, 2] = tx  # сдвиг по x
+    T[1, 2] = ty  # сдвиг по y
+    return T
+
+def forward_kinematics(link_lengths, angles_deg):
+    T_total = np.eye(3)
+    points = []
+    for length, angle in zip(link_lengths,angles_deg):
+        tx = length*np.cos(np.radians(angle))
+        ty = length*np.sin(np.radians(angle))
+        T = transform_matrix(angle, tx, ty)
+        T_total = T_total @ T
+        x_coordinates = T_total[0,2]
+        y_coordinates = T_total[1, 2]
+        points.append((x_coordinates,y_coordinates))
+    return points
